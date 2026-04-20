4b5163a2e63dbd6aa043680e61dfe58069b34f5b
XLewis20 <xavionlewis10@gmail.com>
2026-04-20 01:22:13 -0400

    Add optional steamID to HomePage constructor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,12 @@ import './genre_questionnaire_page.dart';
 import './auth_page.dart';
 import './history_page.dart';
 import './swipe_page.dart';
+import 'package:play_m8/storage/local_store.dart';
 
 class HomePage extends StatefulWidget {
   final String username;
-  const HomePage({Key? key, required this.username}) : super(key: key);
+  final String? steamID;
+  const HomePage({Key? key, required this.username,this.steamID}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -43,7 +45,8 @@ class _HomePageState extends State<HomePage>{
     setState(() {
       _loading = false;
     });
-    }
+  }
+
 
   Future<void> history() async {
     setState(() {
@@ -70,15 +73,12 @@ class _HomePageState extends State<HomePage>{
 
     if (!mounted) return;
     // Navigate to genres page(very important section for page switching)
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (_) => const SwipePage()),
-    );
+    context.push('/swipe');
     setState(() {
       _loading = false;
     });
   }
+
   Future<void> authPage() async {
     setState(() {
       _loading = true;
@@ -97,114 +97,115 @@ class _HomePageState extends State<HomePage>{
     });
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment(0.35, 0.9),
-            child: Text("PlayM8 Home",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment(0.35, 0.9),
+          child: Text("PlayM8 Home",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
             ),
           ),
-          backgroundColor: Colors.redAccent,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.logout,
-                  color: Colors.white),
-              onPressed: () {
-                  authPage();
-              },
+        ),
+        backgroundColor: Colors.redAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout,
+                color: Colors.white),
+            onPressed: () async {
+              await LocalStore.resetAll();
+              authPage();
+            },
+          ),
+        ],
+      ),
+
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  browseGenre();
+                },
+                icon: Icon(Icons.category,color: Colors.white),
+                label: Text("Hello $userName lets browse!!!"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                  foregroundColor: Colors.black,
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+
+            // Quick Actions
+            Text(
+              "Quick Actions",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                children: [
+                  buildCard(Icons.category, "Genres", context,browseGenre),
+                  buildCard(Icons.person, "Browse", context,gameBrowse),
+                  buildCard(Icons.menu_book_rounded, "Library", context,history),
+                  buildCard(Icons.settings, "Settings", context,browseGenre),
+
+                ],
+              ),
             ),
           ],
         ),
-
-        body: Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    browseGenre();
-                  },
-                  icon: Icon(Icons.category,color: Colors.white),
-                  label: Text("Hello $userName lets browse!!!"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    foregroundColor: Colors.black,
-                    padding: EdgeInsets.symmetric(vertical: 14),
-                    textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 25),
-
-
-              // Quick Actions
-              Text(
-                "Quick Actions",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-
-              SizedBox(height: 10),
-
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
-                  children: [
-                    buildCard(Icons.category, "Genres", context,browseGenre),
-                    buildCard(Icons.person, "Browse", context,gameBrowse),
-                    buildCard(Icons.menu_book_rounded, "Library", context,history),
-                    buildCard(Icons.settings, "Settings", context,browseGenre),
-
-                  ],
-                ),
-              ),
-            ],
-          ),
-          color: Colors.white,
-        ),
-      );
-    }
-//Function for easily making cards
-    Widget buildCard(IconData icon, String title, BuildContext context, Future<void> Function () navigate) {
-      return Card(
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        color: Colors.redAccent,
-        child: InkWell(
-          onTap: navigate,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, size: 40, color: Colors.white),
-              SizedBox(height: 10),
-              Text(title,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30
-                  )),
-            ],
-
-          ),
-        ),
-      );
-    }
+        color: Colors.white,
+      ),
+    );
   }
+//Function for easily making cards
+  Widget buildCard(IconData icon, String title, BuildContext context, Future<void> Function () navigate) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      color: Colors.redAccent,
+      child: InkWell(
+        onTap: navigate,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 40, color: Colors.white),
+            SizedBox(height: 10),
+            Text(title,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30
+                )),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
