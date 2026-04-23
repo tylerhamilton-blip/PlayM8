e71a28ae41ad73510de22e3e524483d22d304f71
wallpow <60298593+walliemoid@users.noreply.github.com>
2026-04-23 11:11:23 -0400

    Add files via upload

---
+++ ui.theme/Color.kt
@@ -0,0 +1,39 @@
+package com.example.playm8_questionnaire.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/*
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Red1 = Color(0xFFFF5A5F)
+ */
+
+// Primary button color
+val Red1 = Color(0xFFFF5A5F)
+
+// Secondary softer red
+val Red2 = Color(0xFFFF7A7F)
+
+// Background (light grayish)
+val BackgroundLight = Color(0xFFF7F7F7)
+
+val SurfaceWhite = Color(0xFFFFFFFF)
+
+// Text colors
+val TextPrimary = Color(0xFF000000)     // main text
+val TextSecondary = Color(0xFF6E6E6E)   // gray text
+
+// Ui stuff let me cook
+val ChipBackground = Color(0xFFF0F0F0)
+val DividerGray = Color(0xFFE0E0E0)
+
+//Dark Mode
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val TextLight = Color(0xFFEAEAEA)
